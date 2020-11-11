f548bcde5044ada06d5490964bcf9ba5435d5668
Stephanie Wynn <stephsmac@Stephanies-MacBook-Pro-2.local>
2020-11-11 13:37:31 -0800

    fixing issues

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-var character = 
-document.getElementById("character");
+var character = document.getElementById("character");
 var block = document.getElementById("block");
+var counter = 0;
+
+var count = document.getElementById("count")
 function jump(){
     if(character.classList != "animate"){
         character.classList.add("animate");
@@ -8,12 +10,12 @@ function jump(){
     setTimeout(function(){
         character.classList.remove("animate");
     },500);
+    counter += 1
+    count.innerHTML = counter
 }
 var checkDead = setInterval(function(){
-    var characterTop =
-    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    var blockLeft =
-    parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft<20 && blockLeft>0 && characterTop>=130){
         block.style.animation = "none";
         block.style.display = "none";
