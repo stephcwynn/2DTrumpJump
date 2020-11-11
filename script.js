e965e436eb42f0567b323c34b813076d73ec0474
Stephanie Wynn <stephsmac@Stephanies-MacBook-Pro-2.local>
2020-11-11 10:36:12 -0800

    updating jump animations

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ var character =
 document.getElementById("character");
 var block = document.getElementById("block");
 function jump(){
-    character.classList.add("animate");
+    if(character.classList != "animate"){
+        character.classList.add("animate");
+    }
+    
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500);
 }
